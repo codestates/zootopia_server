03f20590c3cb6a7377b4123c335681d5d08cec77
Wonjun Kang <michael.wonjun.kang@gmail.com>
2020-11-30 16:29:38 +0900

    [dev] main-server / add controllers for userinfo edit

--- main_server/controllers/user/photo.js
+++ main_server/controllers/user/photo.js
@@ -1,5 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../../utilities/multer-transform');
+const { User } = require('../../models');
 //
 module.exports = {
   // change photo
-  patch: (req, res) => {},
+  post: router.use('/', upload.single('image'), async (req, res) => {
+    const userId = req.userId;
+    const { transforms } = req.file;
+
+    const photos = {
+      photo: transforms.filter((el) => el.id === 'original')[0].location,
+      thumbnail: transforms.filter((el) => el.id === 'thumbnail')[0].location,
+    };
+
+    const photoUpdated = await User.update(
+      {
+        ...photos,
+      },
+      {
+        where: {
+          id: userId,
+        },
+      },
+    );
+
+    // console.log(photoUpdated[0]);
+    if (photoUpdated[0] === 1) {
+      res.status(201).json({ ...photos });
+    } else {
+      res.status(400).json({ error: '400' });
+    }
+  }),
 };
