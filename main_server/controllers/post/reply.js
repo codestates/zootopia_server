16af982f94c9dae744aab8a75a8d522da77c83f7
Wonjun Kang <michael.wonjun.kang@gmail.com>
2020-11-30 14:16:53 +0900

    [dev] main-server / add reply controller

--- main_server/controllers/post/reply.js
+++ main_server/controllers/post/reply.js
@@ -1,15 +1,63 @@
+const { Reply } = require('../../models');
 //
 module.exports = {
-  // new reply
-  post: (req, res) => {
-    //
+  // new reply *
+  post: async (req, res) => {
+    const userId = req.userId;
+    const { commentId, text } = req.body;
+
+    try {
+      const replyCreated = await Reply.create({ userId, commentId, text });
+      res.status(201).json({ replyId: replyCreated.id });
+      //
+    } catch (error) {
+      throw error;
+    }
   },
-  // update reply
-  patch: (req, res) => {
-    //
+
+  // update reply *
+  patch: async (req, res) => {
+    const userId = req.userId;
+    const { replyId, text } = req.body;
+
+    try {
+      const replyUpdated = await Reply.update(
+        {
+          text,
+        },
+        {
+          where: {
+            id: replyId,
+            userId,
+          },
+        },
+      );
+      if (replyUpdated[0] !== 1) {
+        return res.status(403).json({ error: '403 Forbidden' });
+      }
+      res.status(201).json({ msg: 'reply updated' });
+      //
+    } catch (error) {
+      throw error;
+    }
   },
-  // delete reply
-  delete: (req, res) => {
-    //
+
+  // delete reply *
+  delete: async (req, res) => {
+    const userId = req.userId;
+    const { replyId } = req.body;
+
+    try {
+      const replyDeleted = await Reply.destroy({
+        where: { id: replyId, userId },
+      });
+      if (replyDeleted !== 1) {
+        return res.status(403).json({ error: '403 Forbidden' });
+      }
+      res.status(200).json({ msg: 'reply deleted' });
+      //
+    } catch (error) {
+      throw error;
+    }
   },
 };
